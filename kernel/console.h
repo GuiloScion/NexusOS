b10d3364abccb6330c5a7e20457a06e4e7796089
Noah Parsons <nomapa223@gmail.com>
2026-05-12 19:29:44 -0600

    Update console.h

--- kernel/console.h
+++ kernel/console.h
@@ -9,5 +9,6 @@ void console_putc(char c);
 void console_puts(const char *s);
 void console_put_hex(uint64_t v);   /* prints "0x" + 16 hex digits */
 void console_put_dec(uint64_t v);
+bool console_try_getc(char *out);   /* non-blocking serial read; true if a byte was read */
 
 #endif
